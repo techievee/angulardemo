d1f4f091b604c1ce2f3cccf06834655f2281efbe
techievee <techievee@gmail.com>
2019-09-23 11:27:19 +1000

    Fix the image for kubernetes deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  serverUrl: 'http://localhost:8888',
+  production: true,
+  serverUrl: 'http://minikube:30112',
 };
 /*
  * For easier debugging in development mode, you can import the following file
